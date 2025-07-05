382c77bbbac19b76da98d2b3ce56759f034ced68
connsteele <35544211+connsteele@users.noreply.github.com>
2025-07-05 13:35:45 -0700

    New header UI allows for project creation (item issue read desc)
    Issue right now where creating a new item makes it go into all cards

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ import { createProjectView } from "./js/projectView";
 import { createProjectController } from "./js/projectController";
 
 //--- General Setup
-let emitter = mitt();
-let projectName = "Test Project"
-let model = createProjectModel(projectName, emitter);
-let view = createProjectView(projectName);
-let controller = createProjectController(model, view, emitter);
+let counter = 0;
 
+//--- Event setup for project creation
+document.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.id === "btn-create") {
+        let emitter = mitt();
+        const inputName = document.querySelector("#project-name");
+        let projectName = inputName.value !== "" ? inputName.value : `Project ${counter++}`;
+        let model = createProjectModel(projectName, emitter);
+        let view = createProjectView(projectName);
+        let controller = createProjectController(model, view, emitter);
+    }
+});
